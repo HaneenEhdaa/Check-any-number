3d28a218e44f4d9fcdf3fcb6ffc68a37b0260e43
HaneenEhdaa <116807965+HaneenEhdaa@users.noreply.github.com>
2024-12-21 01:03:54 +0200

    update

---
+++ check_number.dart
@@ -0,0 +1,34 @@
+
+//Haneen Ehdaa
+void main(List<String> arguments) {
+int mynumber =5 ;
+int mynumber2 = -10 ;
+
+if (mynumber > 0){
+print("$mynumber is positive");
+}
+else {
+  print("$mynumber is negative ");
+}
+if (mynumber %2 ==0){
+  print("$mynumber is even ");
+}
+else{
+  print("$mynumber is odd ");
+}
+
+print("-----------------------------------------------------------------");
+
+if (mynumber2 > 0){
+print("$mynumber2 is positive");
+}
+else {
+  print("$mynumber2 is negative ");
+}
+if (mynumber2 %2 ==0){
+  print("$mynumber2 is even ");
+}
+else{
+  print("$mynumber2 is odd ");
+}
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Problem Statement:
+Title: Check any number positive or negative and even or odd
+Description: Enter any integer number
+Use the condition to check it's positive or negative, then
+use the condition to check if it's even or odd
+
+
